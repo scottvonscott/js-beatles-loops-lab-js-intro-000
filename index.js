2c4e254a69266a5f9ae3a74322503355bf57c578
Scott Keplinger <skeplin17@gmail.com>
2020-03-19 22:50:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var fab4 = [];
+  for (let i = 0; i < musicians.length; i++); {
+   fab4.push(`${musicians[i]} plays ${instruments[i]}`);
+}
+  return fab4;
+}
+
+
+// function theBeatlesPlay(musicians, instruments) {
+//   var fab4 = [];
+//   var i = 0;
+//   for (i = 0; i < musicians.length; i++); {
+//    fab4.push(`${musicians[i]} plays ${instruments[i]}`);
+// }
+//   return fab4;
+// }
