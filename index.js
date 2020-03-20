8ea199df33d8b2896e8d184d45eee50537fe22d1
Scott Keplinger <skeplin17@gmail.com>
2020-03-20 23:07:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
 // add solution here
 function theBeatlesPlay(musicians, instruments) {
   var fab4 = [];
-  for (let i = 0; i < musicians.length; i++); {
+  for (let i = 0; i < musicians.length; i++) {
    fab4.push(`${musicians[i]} plays ${instruments[i]}`);
-}
+ };
   return fab4;
 }
 
+function johnLennonFacts(facts) {
+  var excitedfacts = [];
+  let i = 0
+  while (i < facts.length) {
+    excitedfacts.push(`${facts[i]}!!!`)
+    i += 1
+  }
+  return excitedfacts
+}
 
-// function theBeatlesPlay(musicians, instruments) {
-//   var fab4 = [];
-//   var i = 0;
-//   for (i = 0; i < musicians.length; i++); {
-//    fab4.push(`${musicians[i]} plays ${instruments[i]}`);
-// }
-//   return fab4;
-// }
+function iLoveTheBeatles(num) {
+  var love =[]
+  let i = 0
+  num++
+  do {
+    love.push("I love the Beatles!")
+  } while(num < 15)
+    return love
+}
